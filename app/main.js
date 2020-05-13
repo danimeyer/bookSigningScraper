d08766b7d849c3f27b7240770cdb0af44172bb51
Dani <dani.meyer.nd@gmail.com>
2020-05-13 15:58:26 -0400

    bug: missing bablrc file

--- app/main.js
+++ app/main.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../public/index.css'
-
 ReactDOM.render(
   <div>Hello, world!</div>,
   document.getElementById('app') // make sure this is the same as the id of the div in your index.html
