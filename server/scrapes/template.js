74bd7eedcfb6cce72a5af84bd05f3171946efe42
Dani <dani.meyer.nd@gmail.com>
2020-06-17 14:40:15 -0400

    feature: scrape template

---
+++ server/scrapes/template.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+const Event = require('../db/event');
+
+const url = '';
+
+(async() => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    let events = await page.evaluate(() => {
+      // Grab title, date, time, url
+      let title
+
+      let eventArray = [];
+
+      for (let i = 0; i < title.length; i++) {
+        eventArray[i] = {
+          title: '',
+          url: '',
+          date: '',
+          time: '',
+          bookstore: '',
+        }
+      }
+
+      return eventArray;
+    })
+
+    console.log(events); // Remove after testing
+
+    // await events.map(async (event) => {
+    //   try {
+    //     return await Event.create(event);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // })
+
+    // console.log('Scrape successful');
+
+  } catch (error) {
+    console.error(error)
+  }
+})
